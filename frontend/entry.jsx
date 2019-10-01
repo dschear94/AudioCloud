de652398d3ae53520f9b9c9b29abc79133f680e4
derek schear <dschear94@gmail.com>
2019-10-01 18:26:54 -0400

    Continue work on auth

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Root from "./components/root"
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
-    const root = document.getElementById("root");
-    ReactDOM.render(<h1>AudioCloud - Development</h1>, root);
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            session: { id: window.currentUser.id },
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+    const root = document.getElementById("yo");
+    ReactDOM.render(<Root store={store}/>, root);
 });
