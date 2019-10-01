72f0f5301d4b56280449452f6a2597ebf6122142
derek schear <dschear94@gmail.com>
2019-10-01 15:23:55 -0400

    Work on migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, :session_token, presence: true, uniqueness: true
+    validates :username, :email, :session_token, presence: true, uniqueness: true
     validates :password_digest, presence: true
     validates :password, length: { minimum: 6, allow_nil: true }
     after_initialize :ensure_session_token
