83aaa5a77dd121496992f832b26b0a5c3fe5040a
derek schear <dschear94@gmail.com>
2019-10-04 17:16:00 -0400

    add demo to seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+demo = User.create({username: "derek", password:"123456", email:"123@aol.com", gender:"male", age: 8});
