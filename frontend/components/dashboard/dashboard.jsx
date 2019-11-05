6e2df321d85b20a3fc39307ae9d3c45dc64d4881
derek schear <dschear94@gmail.com>
2019-11-05 11:55:57 -0500

    update profile dashboard navlinks

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { withRouter, Link } from 'react-router-dom';
+import { 
+    withRouter, 
+    Link,
+    NavLink
+} from 'react-router-dom';
 
 class Dashboard extends React.Component {
     constructor(props) {
@@ -12,7 +16,105 @@ class Dashboard extends React.Component {
     render() {
         return (
             <div>
-                In Progress
+                <div className="user-hero">
+                    <div className="profileHeader">
+                        <div className="phEdit">
+                            <div className="phChooserWrapper">
+                                <button className="headerImageEditBtn">
+                                    {/* update for current/noncurrent user */}
+                                    Update Image
+                                </button>
+                            </div>
+                        </div>
+                        <div className="phInfo">
+                            <div className="phInfoAvatarContainer">
+                                <div className="phAvatarImage">
+                                    {/* image goes here inside span*/}
+                                    <div className="phAvatarBtn">
+                                        <button className="imagePicker">
+                                            Update Image
+                                        </button>
+                                        <input 
+                                        className="hiddeninput" 
+                                        type="file"
+                                        style={{opacity: "0"}}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="phInfoContentContainer">
+                                <h3 className="phInfo-username">
+                                    {/* username from props */}
+                                </h3>
+                            </div>
+                        </div>
+                        <div className="phBG">
+                            {/* BG goes here in div */}
+                        </div>
+                    </div>
+                </div>
+                <div className="dashboard-vertical-bar">
+                    <div className="userInfoBar">
+                        <div className="userInfoBarTabs">
+                            <ul className="userInfoBarTabsUL">
+                                <li className="UIBTLI">
+                                    <NavLink
+                                        exact to={`/${this.props.artist}`}
+                                        activeClassName="UIBTLI-active"
+                                        >
+                                        All
+                                    </NavLink>
+                                </li>
+                                <li className="UIBTLI">
+                                    <NavLink
+                                        exact to={`/${this.props.artist}/toptracks`}
+                                        activeClassName="UIBTLI-active"
+                                    >
+                                        Top Tracks
+                                    </NavLink>
+                                </li>
+                                <li className="UIBTLI">
+                                    <NavLink
+                                        exact to={`/${this.props.artist}/tracks`}
+                                        activeClassName="UIBTLI-active"
+                                    >
+                                        Tracks
+                                    </NavLink>
+                                </li>
+                                <li className="UIBTLI">
+                                    <NavLink
+                                        exact to={`/${this.props.artist}/albums`}
+                                        activeClassName="UIBTLI-active"
+                                    >
+                                        Albums
+                                    </NavLink>
+                                </li>
+                                <li className="UIBTLI">
+                                    <NavLink
+                                        exact to={`/${this.props.artist}/sets`}
+                                        activeClassName="UIBTLI-active"
+                                    >
+                                        Playlists
+                                    </NavLink>
+                                </li>
+                                <li className="UIBTLI">
+                                    <NavLink
+                                        exact to={`/${this.props.artist}/reposts`}
+                                        activeClassName="UIBTLI-active"
+                                    >
+                                        Reposts
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="userInfoBarBtns">
+
+                        </div>
+                    </div>
+                </div>
+                <div className="dashboard-content-container">
+
+                </div>
             </div>
         );
     }
--- frontend/components/dashboard/dashboard_container.js
+++ frontend/components/dashboard/dashboard_container.js
@@ -1,13 +1,18 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import Dashboard from './dashboard';
 import { fetchTracks } from '../../actions/track_actions';
 import { receiveCurrentTrack } from '../../actions/current_track_actions';
 
 
 const msp = (state, ownProps) => {
-    debugger
+    let artist = ownProps.match.params.artist ? 
+    ownProps.match.params.artist :
+    state.entities.artists ?
+    state.entities.artists.username : "";
+
     return {
-        // tracks: Object.values(state.entities.tracks) || []
+        artist: artist || ""
     }
 };
 
@@ -17,4 +22,4 @@ const mdp = dispatch => ({
     // receiveCurrentTrack: track => dispatch(receiveCurrentTrack(track))
 });
 
-export default connect(msp, mdp)(Dashboard);
+export default withRouter(connect(msp, mdp)(Dashboard));
