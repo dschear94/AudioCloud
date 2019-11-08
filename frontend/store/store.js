7c04c8c4206184376b37371347aaee687aa733e3
derek schear <dschear94@gmail.com>
2019-11-08 10:40:59 -0500

    add redux devtools extension

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from '../reducers/root_reducer';
 
@@ -8,7 +9,8 @@ const configureStore = (preloadedState = {}) => (
     createStore(
         rootReducer,
         preloadedState,
-        applyMiddleware(thunk, logger)
+        // applyMiddleware(thunk, logger),
+        composeWithDevTools(applyMiddleware(thunk, logger))
     )
 );
 
