0f25ab68ea8131bbd4088de3b17ebce6c4f0e15c
derek schear <dschear94@gmail.com>
2019-11-21 13:58:53 -0500

    update rack-cors

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,8 @@ module AudioCloud
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: :any
-        # [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options]
+        
       end
     end
 
